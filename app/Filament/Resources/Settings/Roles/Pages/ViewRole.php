568ea178af0afe6fb4ee45094f3ec61511502463
codeghor <hafij.to@gmail.com>
2025-09-19 10:09:37 +0600

    Add Role management functionality with Create, Edit, List, and View pages, along with RolePolicy for authorization

---
+++ app/Filament/Resources/Settings/Roles/Pages/ViewRole.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\Settings\Roles\Pages;
+
+use App\Filament\Resources\Settings\Roles\RoleResource;
+use Filament\Actions\EditAction;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewRole extends ViewRecord
+{
+    protected static string $resource = RoleResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            EditAction::make(),
+        ];
+    }
+}
